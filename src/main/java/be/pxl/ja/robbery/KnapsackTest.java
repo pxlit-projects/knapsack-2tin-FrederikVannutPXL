f06f6b53a489317a46b2bfb45be03361743117b0
FrederikVannutPXL <43776401+FrederikVannutPXL@users.noreply.github.com>
2022-10-04 15:34:54 +0200

    oef1 extra tests

--- src/main/java/be/pxl/ja/robbery/KnapsackTest.java
+++ src/main/java/be/pxl/ja/robbery/KnapsackTest.java
@@ -1,4 +1,29 @@
 package be.pxl.ja.robbery;
 
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
 public class KnapsackTest {
+    @Test
+    public void getCurrentWeighttest(){
+        Knapsack knapsack = new Knapsack(100);
+        Shop shop = new Shop();
+        shop.add(new Product("stereo", 30, 3000));
+        shop.add(new Product("tafel", 40, 6000));
+        KnapsackUtil.fill(knapsack, shop);
+        Assertions.assertEquals(70,knapsack.getCurrentWeight());
+    }
+    @Test
+    public void addFunctionSuccesfullyAdds() throws KnapsackFullException {
+        Knapsack knapsack = new Knapsack(100);
+        Product product = new  Product("stereo", 30, 3000);
+        Product product2 = new  Product("tafel", 50, 5000);
+        knapsack.add(product);
+        knapsack.add(product2);
+        Assertions.assertEquals(2,knapsack.getItems().size());
+    }
+    @Test
+    public void addFunctionFails() throws KnapsackFullException {
+
+    }
 }
